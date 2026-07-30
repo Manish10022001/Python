5ceb1980a7e375ea793effef3cf6c48318cf9a91
Manish10022001 <manishshirsat2850@gmail.com>
2026-07-30 23:13:15 +0530

    pre-defined functions of set -3

--- DataTypes/Set/functions/symmetric_difference.py
+++ DataTypes/Set/functions/symmetric_difference.py
@@ -1,3 +1,5 @@
+# This Function removes the common elements from setobj1 and setobj2 and Takes the Remaining Elements from Both setobj1 and setobj2 and place them in setobj1 and setobj3 contains None
+
 s1= {10,20,30,40}
 s2= {10,20,35,45}
 s3= s1.symmetric_difference(s2) # {35, 40, 45, 30}
