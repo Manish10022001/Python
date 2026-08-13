dbf931fa183cbb2eaa5970aefcbc98c9b0e84a47
Manish10022001 <manishshirsat2850@gmail.com>
2026-08-13 20:48:49 +0530

    Relational Operators

---
+++ Operators/Relational/RelationalOp.py
@@ -0,0 +1,13 @@
+a,b=int(input("Enter value of a: ")), int(input("Enter value of b: "))
+
+print("*"*50)
+
+print("\t{}>{} = {}".format(a,b, a>b))
+print("\t{}<{} = {}".format(a,b,a<b))
+print("\t{}=={} = {}".format(a,b, a==b))
+print("\t{}!={} = {}".format(a,b,a!=b))
+print("\t{}>={} = {}".format(a,b, a>=b))
+print("\t{}<={} = {}".format(a,b,a<=b))
+
+
+print("*"*50)
